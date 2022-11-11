e02616d9a5b218d09589b5d21e69e2eddaeb845e
MamboBryan <mambobryan@gmail.com>
2022-11-11 17:40:32 +0300

    fixes(auth):not updating state

--- app/src/main/java/com/steve_md/testapp/ui/fragments/auth/LoginAccountFragment.kt
+++ app/src/main/java/com/steve_md/testapp/ui/fragments/auth/LoginAccountFragment.kt
@@ -5,84 +5,142 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.*
-import androidx.navigation.Navigation
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.steve_md.testapp.R
-import com.steve_md.testapp.data.requests.LoginRequest
-import com.steve_md.testapp.data.responses.LoginResponse
 import com.steve_md.testapp.databinding.FragmentLoginAccountBinding
 import com.steve_md.testapp.utils.Resource
 import com.steve_md.testapp.utils.toast
 import com.steve_md.testapp.viewmodel.AuthViewModel
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.collectLatest
+import timber.log.Timber
 
 
 class LoginAccountFragment : Fragment() {
 
-    private lateinit var binding: FragmentLoginAccountBinding
+    // According to the docs this is a nice way get binding
+    private var _binding: FragmentLoginAccountBinding? = null
+    private val binding get() = _binding!!
 
-    //private val loginViewModel: AuthViewModel by viewModels()
-    private val loginViewModel = activity?.let {
-        ViewModelProvider(it)
-    }?.get(AuthViewModel::class.java)
+    private val viewModel: AuthViewModel by viewModels()
+//    private val viewModel = activity?.let {
+//        ViewModelProvider(it)
+//    }?.get(AuthViewModel::class.java)
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
-        binding = FragmentLoginAccountBinding.inflate(layoutInflater, container, false)
-
-
-        binding.dontHaveAccountSignUpTextView.setOnClickListener {
-            findNavController().navigate(R.id.action_loginAccountFragment_to_createAccountFragment)
-        }
-
-
-        val loginRequest = LoginRequest(
-            binding.inputEmail.editableText.toString(),
-            binding.inputPassword.editableText.toString()
-        )
-
-        binding.buttonLogin.setOnClickListener {
-
-            if (binding.inputEmail.text.isNullOrEmpty() && binding.inputPassword.text.isNullOrEmpty()) {
-                toast("Enter credentials")
-            }
-            else {
-            lifecycleScope.launch {
-                loginViewModel?.postToLogin(loginRequest)
-                    ?.collect {
-                        when (it) {
-                            is Resource.Success<*> -> {
-                                toast("Successful Login")
-                                findNavController().navigate(R.id.action_loginAccountFragment_to_homeFragment)
-                            }
-                            is Resource.Error -> {
-                                toast("Error occurred")
-                            }
-                        }
-                    }
-
-            }
-
-
-        }
-        }
-
+        _binding = FragmentLoginAccountBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.dontHaveAccountSignUpTextView.setOnClickListener {
+            navigateToCreateAccount()
+        }
+
+        binding.buttonLogin.setOnClickListener {
+            if (isValidCredentials()) loginUser()
+            else toast("Enter valid credentials")
+        }
+
+        lifecycleScope.launchWhenResumed {
+            viewModel.loginResult.collectLatest {
+                when (it) {
+                    Resource.Loading -> {
+                        toast("Loading")
+                    }
+                    is Resource.Error -> {
+                        toast("Couldn't log in")
+                    }
+                    is Resource.Success -> {
+                        toast("Successfully Logged In")
+                        navigateToHome()
+                    }
+                    null -> {}
+                }
+            }
+        }
+
+    }
+
+    private fun isValidCredentials(): Boolean {
+        return binding.inputEmail.text.isNullOrEmpty().not().also {
+            if (!it) binding.txtLayEmailAdd.error = "Invalid Email"
+        } && binding.inputPassword.text.isNullOrEmpty().not().also {
+            if (!it) binding.txtLayPassSignup.error = "Invalid Password"
+        }
+    }
+
+    private fun navigateToHome() {
+        findNavController().navigate(R.id.action_loginAccountFragment_to_homeFragment)
+    }
+
+    private fun navigateToCreateAccount() {
+        findNavController().navigate(R.id.action_loginAccountFragment_to_createAccountFragment)
+    }
+
+    private fun loginUser() {
+        Log.i("VAVAVA","VIEWMODEL IS nUll -> ${viewModel == null}")
+        viewModel.login(binding.inputEmail.text.toString(), binding.inputPassword.text.toString())
+    }
+
+//    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View {
+//        // Inflate the layout for this fragment
+//        binding = FragmentLoginAccountBinding.inflate(layoutInflater, container, false)
+//
+//
+//        binding.dontHaveAccountSignUpTextView.setOnClickListener {
+//            findNavController().navigate(R.id.action_loginAccountFragment_to_createAccountFragment)
+//        }
+//
+//
+//        val loginRequest = LoginRequest(
+//            binding.inputEmail.editableText.toString(),
+//            binding.inputPassword.editableText.toString()
+//        )
+//
+//        binding.buttonLogin.setOnClickListener {
+//
+//            if (binding.inputEmail.text.isNullOrEmpty() && binding.inputPassword.text.isNullOrEmpty()) {
+//                toast("Enter credentials")
+//            }
+//            else {
+//            lifecycleScope.launch {
+//                loginViewModel?.postToLogin(loginRequest)
+//                    ?.collect {
+//                        when (it) {
+//                            is Resource.Success<*> -> {
+//                                toast("Successful Login")
+//                                findNavController().navigate(R.id.action_loginAccountFragment_to_homeFragment)
+//                            }
+//                            is Resource.Error -> {
+//                                toast("Error occurred")
+//                            }
+//                        }
+//                    }
+//
+//            }
+//
+//
+//        }
+//        }
+//
+//        return binding.root
+//    }
+
 }
-
-
-
 
 
 //
